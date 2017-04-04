3b35d046eae8947a2efc84ed528aaa281b041bcf
Swift Funcional <info@swiftfuncional.com>
2017-04-04 09:04:15 +0200

    Use getUsers in ViewController

--- For-Each.playground/Contents.swift
+++ For-Each.playground/Contents.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 struct User {}
 
 class UserProvider {
@@ -5,5 +7,27 @@ class UserProvider {
 
 	func getUsers(_ process: (User) -> Void) {
 		users.forEach(process)
+	}
+}
+
+class ViewController: UIViewController {
+	@IBOutlet var tableView: UITableView!
+
+	let db = UserProvider()
+
+	var users = [User]()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		db.getUsers { user in
+			users.append(user)
+
+			let indexPath = [IndexPath(row: users.count - 1, section: 0)]
+
+			tableView.beginUpdates()
+			tableView.insertRows(at: indexPath, with: .automatic)
+			tableView.endUpdates()
+		}
 	}
 }
