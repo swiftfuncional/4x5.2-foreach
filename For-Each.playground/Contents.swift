1b97b88abda3646de6a18fb0f6eb77ccdab926fa
Swift Funcional <info@swiftfuncional.com>
2017-04-04 08:54:41 +0200

    ForEach is a high order function

--- For-Each.playground/Contents.swift
+++ For-Each.playground/Contents.swift
@@ -2,6 +2,8 @@ for number in Array(0...10) {
 	print("The number is \(number)")
 }
 
-Array(0...10).forEach {
+let printNumber: (Int) -> Void = {
 	print("The number is \($0)")
 }
+
+Array(0...10).forEach(printNumber)
